72cf5060d71505ccbb9c04d20d8190551ac65b4e
Christian Toscano <chrismarcadopago@gmail.com>
2019-08-15 18:14:46 -0300

    ultimos ajustes

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL: 'https://my-json-server.typicode.com/christianToscano96/JsonServer_CRUD/'
 });
 
 export default clienteAxios;
